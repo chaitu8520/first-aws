0268141ebcc7242c0b3b67c92fbe9485049d18c0
Vardhan7396 <vardhanoggu481@gmail.com>
2026-03-01 19:03:14 +0530

    first commit

---
+++ src/test/java/com/chaitu/first_aws/FirstAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chaitu.first_aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
